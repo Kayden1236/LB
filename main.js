646c89427336934f3cdba36add43877518bb189b
LucasBetolian <lbetolian@gmail.com>
2025-11-24 11:14:54 -0800

    sumry
    struff added

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('Play');
+
+// Add a click event listener
+button.addEventListener('click', function() {
+    messageArea.textContent = 'Button was clicked!';
+})
